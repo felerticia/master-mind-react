2cb3a0afc6bdefce1ee84fc5ada5c9f8a58c2358
Amirhossein Shahbabaee <amirhossein.shahbabaee@citynetwork.eu>
2023-04-04 21:51:02 +0200

    Game Ends

--- src/components/GameEnds/GameEnds.js
+++ src/components/GameEnds/GameEnds.js
@@ -1,10 +1,23 @@
 import './GameEnds.css'
+import { useAppContext }from '../../contexts/Context'
+import Circle from '../Circle/Circle';
+import NewGame from '../Buttons/NewGame';
 
 const GameEnds = () => {
-    return null
+    const [appState] = useAppContext();
+
+    if(!appState.gameState)
+        return
+
     return <div className="GameEnds">
-        <p>You won!</p>
+        <p>You {appState.gameState}!</p>
+        <p>The secret was:</p>
+        <div className='secret'>
+            {appState.secret.map(x => <Circle color={x}/>)}
+        </div>
+        <NewGame/>
     </div>
+
 }
 
 export default GameEnds
